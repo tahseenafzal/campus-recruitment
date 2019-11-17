57bc6d3b53934ea0d37b782bb1a1f73a4caa4e0c
Tehseen Afzal <abuashja@gmail.com>
2019-11-17 19:55:40 +0500

    add job item component

--- client/src/components/pages/jobs/JobItem.js
+++ client/src/components/pages/jobs/JobItem.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from 'react'
 
 const JobItem = () => {
-    return(
-        <h1>This is job item</h1>
-    );
+    return (
+        <div className="card bg-light" > 
+        <h3 className="text-primary text-left">
+            {title}
+        </h3>
+        <ul className="list">
+            {email && (
+                <li>
+                    {description}
+                </li>
+            )}
+            {email && (
+                <li>
+                    {requirement}
+                </li>
+            )}
+        </ul>
+        <p>
+            <button className="btn btn-dark btn-sm">Edit</button>
+            <button className="btn btn-danger btn-sm">Delele</button>
+
+        </p>
+        </div>
+    )
 }
 
-export default JobItem
+export default JobItem 
